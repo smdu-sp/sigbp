9ded690fee56b2443aac9714d4aae01356a49948
NelsonFreitas25 <127761444+NelsonFreitas25@users.noreply.github.com>
2024-05-09 13:43:56 -0300

    Adicionado controle de unidades

--- tabela-unidades.php
+++ tabela-unidades.php
@@ -1,5 +1,194 @@
 <?php
-include_once ('./conexoes/config.php');
-include_once ('header.php');
-include_once ('env.php');
+session_start();
+include_once('verificacao.php');
+include_once('header.php');
+include_once('./conexoes/config.php');
+
+$sql = "SELECT * FROM unidades ORDER BY id ASC";
+$result = $conexao->query($sql) or die($mysqli->error);
 ?>
+<style>
+    .table-container{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-top: 100px !important;
+    }
+
+    .conteudo {
+        height: 100vh;
+    }
+
+    .icon-carrossel {
+        width: 18px;
+    }
+
+    .icon-carrossel-i {
+        width: 16px;
+    }
+
+    .carrossel>a {
+        font-size: 15px;
+    }
+
+    .carrossel>a:hover {
+        text-decoration: none;
+    }
+
+    .carrossel {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+
+    .carrossel-text {
+        text-decoration: none;
+    }
+
+    .carrossel-text:hover {
+        text-decoration: none;
+    }
+
+    .bnt-cadastrar{
+        position: fixed;
+        right: 150px;
+        bottom: 150px;
+    }
+
+
+    @media (max-width: 1600px) {
+        .conteudo {
+            margin-left: 75px;
+        }
+
+        .menu-principal {
+            position: fixed;
+            top: 0;
+            left: -187px;
+            z-index: 999999 !important;
+            transition: all 1s ease;
+        }
+
+
+        .menu-logout {
+            z-index: 1000000 !important;
+        }
+
+        .aparecer {
+            left: 70px !important;
+        }
+
+
+        .menu-button {
+            display: block;
+            cursor: pointer;
+        }
+    }
+</style>
+
+<body>
+    <?php
+    include_once('menu.php');
+    ?>
+    <div class="p-4 p-md-4 conteudo">
+        <div class="carrossel mb-4">
+            <a href="./home.php" class="mb-3 me-1">
+                <img src="./images/icon-casa.png" class="icon-carrossel mt-3" alt="">
+            </a>
+            <img src="./images/icon-avancar.png" class="icon-carrossel-i" alt="icon-avancar">
+            <a href="./termo.php" class="text-primary ms-1 carrossel-text">Listar/Movimentar Bens</a>
+        </div>
+        <select class="form-select" aria-label="Default select example">
+                <option selected>Open this select menu</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+            </select>
+        <div class="conteudo ml-1 mt-4 table-container">
+            <table id="example" class="display table" style="width: 1400px;">
+                <thead class="table-primary">
+                    <tr>
+                        <th>Nome</th>
+                        <th>Sigla</th>
+                        <th>Codigo</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    while ($user_data = mysqli_fetch_assoc($result)) {
+                        
+                        echo "<tr onclick=location.href='unidades.php?id=$user_data[id]' style=' cursor: pointer; background-color:hover: grey;'>";
+                        echo "<td>" . $user_data['unidades'] . "</td>";
+                        echo "<td>" . $user_data['sigla'] . "</td>";
+                        echo "<td>" . $user_data['codigo'] . "</td>";
+                        $tipostatus = [
+                            'Ativo',
+                            'Inativo'
+                        ];
+                        echo "<td>" . $tipostatus[$user_data['statusunidade']] . "</td>";
+                        echo "</tr>";
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+        <div class="hide" id="modal"></div>
+        <a type="button" class="btn btn-primary bnt-cadastrar" href="unidades.php">Cadastrar</a>
+</body>
+<script>
+    function alert(num) {
+        if(num == 1) {
+            const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+            }
+        });
+            Toast.fire({
+                icon: "success",
+                title: "Item movimentado com sucesso!"
+            });
+        } else {
+            const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+            }
+        });
+            Toast.fire({
+                icon: "success",
+                title: "Item alterado com sucesso!"
+            });
+            
+        }
+    }
+    // alert();
+    window.addEventListener('load', function() {
+        var url_string = window.location.href;
+        var url = new URL(url_string);
+        var data = url.searchParams.get("notificacao");
+        console.log(data);
+        if (data == null) {
+            return;
+        } else if (data == 1) {
+            alert(1);
+            window.history.replaceState({}, document.title, window.location.pathname);
+            history.pushState({}, '', 'http://localhost/listaremovimentar.php');
+        } else {
+            alert(2);
+            window.history.replaceState({}, document.title, window.location.pathname);
+            history.pushState({}, '', 'http://localhost/listaremovimentar.php');
+        }
+    })
+</script>
+</html>
--- unidades.php
+++ unidades.php
@@ -1,163 +1,97 @@
 <?php
 session_start();
-include_once ('./conexoes/config.php');
+include_once ('verificacao.php');
 include_once ('header.php');
-include_once ('env.php');
+include_once ('./conexoes/config.php');
 
-$buscar_permisao = "SELECT permissao FROM usuarios WHERE `usuario`='" . strtolower($_SESSION['SesID']) . "';";
-$query_usuario = mysqli_query($conexao, $buscar_permisao);
-$row = mysqli_fetch_assoc($query_usuario);
-$permissao = $row['permissao'];
-if ($permissao != 1) {
-    header('Location: home.php');
-}
+$sql = "SELECT * FROM item ORDER BY idbem ASC";
+$result = $conexao->query($sql) or die($mysqli->error);
 
+isset($_GET['id']) ? $id = $_GET['id'] : $id = null;
 
-$usuario = '';
-$nomefr = '';
-$emailfr = '';
-
-if (isset($_GET['usuario'])) {
-  $usuario = $_GET['usuario'];
-  $search = "samaccountname=" . $usuario;
-
-  $ds = ldap_connect($server);
-  ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3);
-  $r = ldap_bind($ds, $user, $psw);
-
-  $sr = ldap_search($ds, $dn, $search);
-  $data = ldap_get_entries($ds, $sr);
-
-  for ($i = 0; $i < $data["count"]; $i++) {
-    $nomefr = $data[$i]["givenname"][0] . " " . $data[$i]["sn"][0];
-    $emailfr = strtolower($data[$i]["mail"][0]);
-  }
-}
-
-if (isset($_POST['submit'])) {
-  $usuario = $_POST['loginRede'];
+if ($id != null && isset($_POST['submit'])) {
   $nome = $_POST['nome'];
-  $permissao = $_POST['permissao'];
+  $codigo = $_POST['codigo'];
+  $sigla = $_POST['sigla'];
   $status = $_POST['status'];
 
-  $result = mysqli_query($conexao, "INSERT INTO usuarios(usuario, nome, permissao, statususer) VALUES ('$usuario', '$nome', '$permissao', '$status')");
+  $result = mysqli_query($conexao, "UPDATE unidades SET unidades = '$nome', sigla = '$sigla', codigo = '$codigo', statusunidade = $status WHERE id = '$id'");
 
-  header('Location: cadastrodeusuario.php?notificacao=true');
+} else {
+  if (isset($_POST['submit'])) {
+    $nome = $_POST['nome'];
+    $codigo = $_POST['codigo'];
+    $sigla = $_POST['sigla'];
+    $status = $_POST['status'];
+
+    $result = mysqli_query($conexao, "INSERT INTO unidades(unidades, sigla, codigo, statusunidade) VALUES ('$nome', '$sigla', '$codigo', $status)");
+  }
 }
 ?>
+
+<style>
+  .card {
+    padding: 30px 20px;
+    margin-bottom: 30px;
+  }
+</style>
 
 <body>
   <?php
   include_once ('menu.php');
   ?>
-  <div class="p-4 p-md-4 pt-3 conteudo">
+  <div class="p-4 p-md-4 pt-3 conteudo container">
     <div class="carrossel mb-2">
       <a href="./home.php" class="mb-3 me-1">
         <img src="./images/icon-casa.png" class="icon-carrossel mt-3" alt="">
       </a>
       <img src="./images/icon-avancar.png" class="icon-carrossel-i" alt="icon-avancar">
-      <a href="./cadastrarbens.php" class="text-primary ms-1 carrossel-text">Cadastro de Unidades</a>
+      <a href="./cadastrarbens.php" class="text-primary ms-1 carrossel-text">Cadastro de Usuários</a>
     </div>
+      
     <h3 class="mb-4 mt-4">Cadastro de Unidades</h3>
     <form method="POST" action="#">
-      <div class="row">
-
-
-        <div class="col-md-8 mb-1">
-          <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label text-muted">Nome</label>
-            <input type="text" class="form-control" id="exampleFormControlInput1" id="inputCadUsuario"
-              placeholder="Nome" name="nome" value="<?php echo $nomefr; ?>" required>
+      <div class="card" style="width: 1400px">
+        <div class="row">
+          <div class="col-md-6 mb-1">
+            <div class="mb-3">
+              <label for="exampleFormControlInput1" class="form-label text-muted">Nome</label>
+              <input type="text" class="form-control" id="exampleFormControlInput1" id="inputCadUsuario"
+                placeholder="Nome" name="nome" required>
+            </div>
+          </div>
+          <div class="col-md-6 mb-1">
+            <div class="mb-3">
+              <label for="exampleFormControlInput1" class="form-label text-muted">codigo</label>
+              <input type="number" class="form-control" id="exampleFormControlInput1" id="inputCadUsuario"
+                placeholder="código" name="codigo" min="0" required>
+            </div>
+          </div>
+          <hr id="cdusuario" style="width: 97%;" class="mb-2">
+          <div class="col-md-6 mb-1">
+            <div class="mb-3">
+              <label for="exampleFormControlInput1" class="form-label text-muted">Sigla</label>
+              <input type="text" class="form-control" id="exampleFormControlInput1" id="inputCadUsuario"
+                placeholder="código" name="sigla" min="0" required>
+            </div>
+          </div>
+          <div class="col-md-6 mb-1">
+            <label for="usuarioCadastro" class="form-label text-muted">Status</label>
+            <div class="input-group">
+              <div class="input-group-text" style="background-color: transparent;"><img src="./images/icon-status.png"
+                  alt="" class="imgCadastro"></div>
+              <select class="form-select" name="status" required>
+                <option value="" hidden="hidden">Selecionar</option>
+                <option value="0">Ativo</option>
+                <option value="1">Desativado</option>
+              </select>
+            </div>
           </div>
         </div>
-        <div class="col-md-4 mb-3">
-          <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label text-muted">codigo</label>
-            <input type="number" class="form-control" id="exampleFormControlInput1" id="inputCadUsuario"
-              placeholder="código" name="nome" min="0" required>
-          </div>
-        </div>
-        <hr id="cdusuario" style="width: 97%;" class="mb-2">
-        <div class="col-md-6 mb-3">
-          <label for="usuarioCadastro" class="form-label text-muted">Unidade</label>
-          <div class="input-group">
-            <div class="input-group-text" style="background-color: transparent;"><img src="./images/unidades.png" alt=""
-                class="imgCadastro"></div>
-            <select class="form-select" name="unidade" required>
-              <option value="" hidden="hidden">Selecionar</option>
-              <option value="ASCOM">ASCOM</option>
-              <option value="ATAJ">ATAJ</option>
-              <option value="ATECC">ATECC</option>
-              <option value="ATIC">ATIC</option>
-              <option value="AUDITÓRIO">AUDITÓRIO</option>
-              <option value="CAF">CAF</option>
-              <option value="CAF/DGP">CAF/DGP</option>
-              <option value="CAF/DLC">CAF/DLC</option>
-              <option value="CAF/DOF">CAF/DOF</option>
-              <option value="CAF/DSUP">CAF/DSUP</option>
-              <option value="CAP">CAP</option>
-              <option value="CAP/ARTHUR SABOYA">CAP/ARTHUR SABOYA</option>
-              <option value="CAP/DEPROT">CAP/DEPROT</option>
-              <option value="CAP/DPCI">CAP/DPCI</option>
-              <option value="CAP/DPD">CAP/DPD</option>
-              <option value="CAP/NÚCLEO DE ATENDIMENTO">CAP/NÚCLEO DE ATENDIMENTO</option>
-              <option value="CASE">CASE</option>
-              <option value="CASE/DCAD">CASE/DCAD</option>
-              <option value="CASE/DDU">CASE/DDU</option>
-              <option value="CASE/DLE">CASE/DLE</option>
-              <option value="CASE/STEL">CASE/STEL</option>
-              <option value="CEPEUC">CEPEUC</option>
-              <option value="CGPATRI">CGPATRI</option>
-              <option value="COMIN">COMIN</option>
-              <option value="COMIN/DCIGP">COMIN/DCIGP</option>
-              <option value="COMIN/DCIMP">COMIN/DCIMP</option>
-              <option value="CONTRU">CONTRU</option>
-              <option value="CONTRU/DACESS">CONTRU/DACESS</option>
-              <option value="CONTRU/DINS">CONTRU/DINS</option>
-              <option value="CONTRU/DLR">CONTRU/DLR</option>
-              <option value="CONTRU/DSUS">CONTRU/DSUS</option>
-              <option value="DEUSO">DEUSO</option>
-              <option value="GABINETE">GABINETE</option>
-              <option value="GEOINFO">GEOINFO</option>
-              <option value="GTEC">GTEC</option>
-              <option value="ILUME">ILUME</option>
-              <option value="PARHIS">PARHIS</option>
-              <option value="PARHIS/DHIS">PHARIS/DHIS</option>
-              <option value="PARHIS/DHMP">PHARIS/DHMP</option>
-              <option value="PARHIS/DPS">PHARIS/DPS</option>
-              <option value="PLANURB">PLANURB</option>
-              <option value="RESID">RESID</option>
-              <option value="RESID/DRGP">RESID/DRGP</option>
-              <option value="RESID/DRPM">RESID/DRPM</option>
-              <option value="RESID/DRU">RESID/DRU</option>
-              <option value="SERVIN">SERVIN</option>
-              <option value="SERVIN/DSIGP">SERVIN/DSIGP</option>
-              <option value="SERVIN/DSIMP">SERVIN/DSIMP</option>
-            </select>
-          </div>
-          
-        </div>
-        <div class="col-md-6 mb-3">
-          <label for="usuarioCadastro" class="form-label text-muted">Status</label>
-          <div class="input-group">
-            <div class="input-group-text" style="background-color: transparent;"><img src="./images/icon-status.png"
-                alt="" class="imgCadastro"></div>
-            <select class="form-select" name="status" required>
-              <option value="" hidden="hidden">Selecionar</option>
-              <option value="0">Ativo</option>
-              <option value="1">Desativado</option>
-
-            </select>
-          </div>
-        </div>
-      
-
-
         <div class="d-flex flex-row-reverse">
           <input type="submit" class="btn btn-primary ml-3 pe-auto mr-2 " id="btn-cadUsuario" name="submit"
-            value="Cadastrar"></input>
-          <input type="button" class="btn btn-light pe-auto" id="btnSair-cadUsuario" name="salvar"
-            value="Cancelar"></input>
+            value="<?php echo isset($_GET['id']) ? 'Atualizar' : 'Cadastrar'?>"></input>
+          <a type="button" class="btn btn-light bnt-cadastrar" href="tabela-unidades.php">Cancelar</a>
         </div>
     </form>
 </body>
@@ -193,9 +127,9 @@ if (isset($_POST['submit'])) {
     if (data == 'true') {
       toast();
       window.history.replaceState({}, document.title, window.location.pathname);
-      history.pushState({}, '', 'cadastrarbens.php');
+      history.pushState({}, '', 'http://localhost/cadastrarbens.php');
     }
   })
-</script>
+</>
 
-</html>
+</html >
